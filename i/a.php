9fd4238c71720321210f62e06c1f1e38f26b29e3
owen47-ux <66010914114@msu.ac.th>
2026-02-10 10:01:14 +0700

    Add files via upload

---
+++ i/a.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>วาสุเทพ ดวงแพงมาตร (โอเว่น)</title>
+</head>
+
+<body>
+
+<h1>งาน i</h1>
+<h1>วาสุเทพ ดวงแพงมาตร (โอเว่น)</h1>
+
+</body>
+</html>
